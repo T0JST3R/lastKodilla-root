b2c54774c1280d673e051032b22960a72f2173b2
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-07-04 21:49:09 +0200

    update

--- src/main/java/com/crud/tasks/repository/MatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crud.tasks.repository;
-
-import com.crud.tasks.domain.Match;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MatchRepository extends CrudRepository<Match , Long> {
-}
