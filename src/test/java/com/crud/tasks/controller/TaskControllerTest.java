0824fdb91dcd94a88a95f3c83c96850a42456fe9
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-08-19 09:29:03 +0200

    thymeleaf added

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -1,10 +1,14 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -26,26 +30,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(TaskController.class)
 public class TaskControllerTest {
     private Task task = new Task(3L, "Test", "Test");
+    private TaskDto taskDto = new TaskDto(3L , "TestDto" , "testt");
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private DbService dbService;
+    @MockBean
+    TaskMapper taskMapper;
 
 
     @Test
     public void shouldSaveTask() throws Exception {
-        when(dbService.saveTask(any())).thenReturn(task);
-        Gson gson = new Gson();
-        String jsonContent = gson.toJson(task);
+        TaskDto taskDto = new TaskDto(
+                1L,
+                "title",
+                "content");
+        Task  task = new Task(1L, "title", "content");
 
+        when(taskMapper.mapToTask(ArgumentMatchers.any(TaskDto.class))).thenReturn(task);
+        when(dbService.saveTask(task)).thenReturn(task);
+        when(taskMapper.mapToTaskDto(task)).thenReturn(taskDto);
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
         //When & Then
         mockMvc.perform(post("/v1/task/createTask")
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
                 .content(jsonContent))
-                .andExpect(jsonPath("$.id", is(3L)))
-                .andExpect(jsonPath("$.name", is("Test")))
-                .andExpect(jsonPath("$.content", is("Test")));
+                .andExpect(jsonPath("$.id",is(1)))
+                .andExpect(jsonPath("$.title",is("title")))
+                .andExpect(jsonPath("$.content", is("content")));
 
     }
 
