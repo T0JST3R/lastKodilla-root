96d68e5ac497f5efb273b3bff84efa061116016f
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-08-19 11:54:12 +0200

    thymeleaf crudApp initialized

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,23 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
+    @Autowired
+    MailCreatorService mailCreatorService;
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
 
     public void send(Mail mail) {
         LOGGER.info("Starting mail preparation... ");
         try {
-            SimpleMailMessage mailMessage = createMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info(" Mail has been sent");
         } catch (MailException e) {
             LOGGER.error(" MAIL HAS BEEN NOT SENT", e.getMessage());
         }
+    }
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailReceiver());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()) , true);
+        };
     }
 
     private SimpleMailMessage createMessage(Mail mail) {
